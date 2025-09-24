d572f99e597ed501e1318531af1cb4dd5d543f6a
lottepohl <lotte.pohl@vliz.be>
2025-09-24 10:35:57 +0200

    source chunks 1-3 successful

---
+++ source_scripts.R
@@ -0,0 +1,12 @@
+# This scrupt sources all code chunks in '02_code'in the correct order
+
+rm(list=ls())
+# Chunk 01
+source("02_code/01_detection_data_to_hourly_presences.R")
+
+# Chunk 02
+source("02_code/02_download_environmental_covariates.R")
+
+# Chunk 03
+source("02_code/03_couple_env_data_to_detections.R")
+
